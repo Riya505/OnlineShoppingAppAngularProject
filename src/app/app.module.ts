3cec9117444c61350204836da8ffad3e4024d10e
Riya505 <riyapk505@gmail.com>
2022-10-09 21:07:43 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OnlineshoppingComponent } from './onlineshopping/onlineshopping.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OnlineshoppingComponent
+    OnlineshoppingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
